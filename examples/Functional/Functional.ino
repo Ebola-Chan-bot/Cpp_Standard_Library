d750348e4bcd084e6bb64b0a4b5ebb3c58eccce7
埃博拉酱 <vhtmfu@outlook.com>
2024-08-13 20:13:06 +0800

    修复deque重定义问题

--- examples/Functional/Functional.ino
+++ examples/Functional/Functional.ino
@@ -1,5 +1,5 @@
 #include <Cpp_Standard_Library.h>
-//演示多个标准库的功能
+//演示多个标准库的功能，无可展示的运行时效果
 #include <chrono>
 #include <algorithm>
 #include <iostream>
@@ -8,15 +8,21 @@
 #include <cmath>
 #include <unordered_map>
 #include <optional>
+#include <map>
+#include <queue>
+#include <set>
+std::map<uint8_t, std::move_only_function<void(std::dynarray<char> &&) const>> ListeningPorts;
+std::queue<std::dynarray<char>> MessageQueue;
+std::set<std::function<void()>> IdleTasks;
 void Translate(std::chrono::hours H) {
   std::cout << std::chrono::duration_cast<std::chrono::seconds>(H + H).count() << std::endl;
 }
-std::vector<std::optional<std::move_only_function<void()const>>> FF;
+std::move_only_function<void()> FF;
 void setup() {
   std::unique_ptr<std::chrono::hours[]> US = std::make_unique_for_overwrite<std::chrono::hours[]>(10);
   std::fill_n(US.get(), 10, std::chrono::hours(1));
   std::chrono::hours H = US[5];
-  FF[0] = [H]() {
+  FF = [H]() {
     Translate(H);
   };
   std::dynarray<int> D(std::log2(1024));
@@ -29,5 +35,5 @@ void setup() {
 #endif
 }
 void loop() {
-  FF[0].value()();
+  FF();
 }
