373d6ada28cbc311d56898a6eca1ab87fdeb311c
埃博拉酱 <vhtmfu@outlook.com>
2025-08-12 21:46:49 +0800

    20250812

--- examples/UnitTests/Functional.cpp
+++ examples/UnitTests/Functional.cpp
@@ -1,5 +1,18 @@
 
 #include <functional>
-void Functional() {
+struct Type
+{
+  Type(Type&&){}
+  Type(int){}
+  Type&operator=(int){}
+};
+struct AsMember
+{
+  Type MemberB=0;
+  //std::move_only_function<void() const> const Member = []() {};
+};
+void Functional()
+{
   std::move_only_function<void() const>{}();
+  AsMember AM;
 }
